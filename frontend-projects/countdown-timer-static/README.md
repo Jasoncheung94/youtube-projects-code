30598e56a248e4ebde0612c5159b096930fdda94
Jason Cheung <jasoncheung94@hotmail.com>
2022-05-22 19:02:48 +0100

    Project: Countdown timer static

---
+++ frontend-projects/countdown-timer-static/README.md
@@ -0,0 +1,8 @@
+# Countdown timer Project
+A countdown timer that uses dates for it's calculations. The deadline is decided by a date in the code.
+
+### Preview
+<img src="https://user-images.githubusercontent.com/32305579/137603266-72ecabc1-7aad-41f8-8889-16432e4f9b0c.png" width=600>
+
+### Helpful links
+* [Unsplash background image](https://unsplash.com/photos/wM70mc7KCqM)
---
+++ frontend-projects/countdown-timer-static/index.js
@@ -0,0 +1,24 @@
+const countdown = () => {
+  // Specify the date and time we are counting down to.
+  const countDate = new Date("Jan 1, 2035 00:00:00").getTime();
+  const now = new Date().getTime();
+  const remainingTime = countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const textDay = Math.floor(remainingTime / day);
+  const textHour = Math.floor((remainingTime % day) / hour);
+  const textMinute = Math.floor((remainingTime % hour) / minute);
+  const textSecond = Math.floor((remainingTime % minute) / second);
+
+  document.querySelector(".day").innerText = textDay > 0 ? textDay : 0;
+  document.querySelector(".hour").innerText = textHour > 0 ? textHour : 0;
+  document.querySelector(".minute").innerText = textMinute > 0 ? textMinute : 0;
+  document.querySelector(".second").innerText = textSecond > 0 ? textSecond : 0;
+};
+
+// should use 500 as setInterval won't always run on time.
+setInterval(countdown, 500);
